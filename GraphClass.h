959d2f1ced8119a17c1ed4a11d9ffc4aff4b4c84
yorny2101 <fundoraluzbetyory@gmail.com>
2022-05-31 20:04:15 -0400

    sustitucion de la clase por funciones independiente

--- GraphClass.h
+++ GraphClass.h
@@ -1,11 +1,12 @@
+#ifndef GRAFO_EN_FUNCION_LIBRARY_H
+#define GRAFO_EN_FUNCION_LIBRARY_H
+
 #include <iostream>                  // for std::cout
 #include <utility>                   // for std::pair
 #include <algorithm>                 // for std::for_each
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
-//#include < boost/graph/properties.hpp>
-
 #include <vector>
 
 using namespace boost;
@@ -19,55 +20,41 @@ struct node_name_t { // crea una nueva propiedad para el vertice
     typedef vertex_property_tag kind;
   };
 
-
   typedef property<node_name_t, std::string> NodeNameProperty;
-  typedef adjacency_list<listS, vecS, undirectedS, NodeNameProperty, property<edge_weight_t, int> > Graph;
+  typedef adjacency_list<listS, vecS, undirectedS, NodeNameProperty, property<edge_weight_t, float> > Graph;
   typedef vector< int > vector_int;
   typedef vector< float > vector_float;
   typedef std::pair<int, int> Edge;
   typedef vector< Edge > vector_edges;
   typedef vector< string > vector_string;
+  typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
 
-class Grafo{
-public:
+  Graph G;
+  property_map<Graph, node_name_t>::type name = get(node_name_t(), G);
 
-// atributos
-    Graph g;
-    int num_edges;
-    vector_edges vector_edge;
-    vector_float weigths;
-    vector_string name_vertex;
-    vector_int index_vertex;
-    property_map<Graph, node_name_t>::type name = get(node_name_t(), g);
-    // METODOS
-    Grafo(){}
-    Grafo(vector_edges edges_input, vector_float weigths_input,
-            vector_int index_vertex_input, vector_string name_node_input){
-
-        add_edges(edges_input, weigths_input);
-        add_name_vertex(index_vertex_input, name_node_input);
-
-        name_vertex.insert(name_vertex.end(), name_node_input.begin(), name_node_input.end() ); // poner condicional para si esta vacio
-        weigths.insert(weigths.end(), weigths_input.begin(), weigths_input.end());
-        index_vertex.insert(index_vertex.end(), index_vertex_input.begin(), index_vertex_input.end());
-        vector_edge.insert(vector_edge.end() , edges_input.begin(), edges_input.end());
-
-    }
-
-    void add_edges(vector_edges vector_edge_p, vector_float weigths_p){
-        for(int i = 0; i < num_edges; i++){
-            add_edge(vector_edge_p[i].first, vector_edge_p[i].second, weigths_p[i], g); // agrega un arco con su respectivo peso
+  void add_edges(Edge arco, float weigth){
+       add_edge(arco.first, arco.second, weigth, G); // agrega un arco con su respectivo peso
      }
 
-}// end of add_edges function
 
-    void add_name_vertex(vector_int index_vertex_p, vector_string name_vertex_p){
-       // property_map<Graph, node_name_t>::type name = get(node_name_t(), g);
 
-        for(auto i: index_vertex_p){ // hay q ver eso
-           name[i] = name_vertex_p[i]; // establece un nombre para cada vertice
-           // si no funciona asi poner  boost::put(name, i, name_vertex[i])
+    void add_name_vertex(int index_vertex_p, string name_vertex_p){
+        boost::put(name, index_vertex_p, name_vertex_p);
         }
 
+    Vertex add_node(){
+     Vertex v1 = boost::add_vertex(G);
+       
     }
-};
+    
+   /* void remove_node(){
+      //clear_vertex()
+      // get the property map for vertex indices
+    typedef property_map<Graph, vertex_index_t>::type IndexMap;
+    IndexMap index = get(vertex_index, G);
+
+    } */
+
+
+
+#endif //GRAFO_EN_FUNCION_LIBRARY_H
